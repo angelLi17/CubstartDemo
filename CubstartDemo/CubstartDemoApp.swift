5004122c385b8986345b5c57ebedb27235a55995
angel <angel_li@berkeley.edu>
2026-09-12 17:56:18 -0700

    Initial Commit

---
+++ CubstartDemo/CubstartDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubstartDemoApp.swift
+//  CubstartDemo
+//
+//  Created by Angel on 9/12/26.
+//
+
+import SwiftUI
+
+@main
+struct CubstartDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CubstartDemoTests/CubstartDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  CubstartDemoTests.swift
+//  CubstartDemoTests
+//
+//  Created by Angel on 9/12/26.
+//
+
+import Testing
+@testable import CubstartDemo
+
+struct CubstartDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
